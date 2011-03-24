4dc4ffd881da2a2049d69aded67c357b4caf8bb4
Eskil Ugelstad <eeu@stormtroopers.no>
2011-03-24 16:46:57 +0100

    Automagisk epost skel

---
+++ epost.bash
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+##
+# Automatisert epost installasjon
+# Rev. 00000001
+# Last edit: Thursday 24 March 2011 
+##
+
+# Kommenter ut når rev er testet. 
+#if [ "$1" != "test" ]; then
+#	echo "Det jobbes med scriptet for øyeblikket vennligst prøv igjen senere"
+#	exit
+#fi
+
+
