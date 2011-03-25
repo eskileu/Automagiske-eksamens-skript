11523b51f1d6954d9e54c21db80ac8e2119b71d6
Eskil Ugelstad <eeu@stormtroopers.no>
2011-03-25 12:16:16 +0100

    Mye nytt. Installasjon av postfix og courier. <3 SED

--- epost.bash
+++ epost.bash
@@ -121,6 +121,11 @@ else
 fi
 
 # Verifiser at brukeren virkelig vil gå videre
+echo "
+Dette skriptet vil automatisere installasjonen av en epost løsning.
+Sørg for at du har riktige innstillinger knyttet til hostname, IP, domene og
+dns før du går videre. Er du usikker så avbryt og dobbelsjekk dine innstillinger!!"
+echo " "
 pause "Om du er sikker trykk ENTER eller avbryt med CTRL+C"
 
 ##
@@ -174,9 +179,71 @@ echo " "
 apt-get update
 apt-get -qy install postfix courier-authdaemon courier-pop-ssl  courier-imap-ssl sasl2-bin procmail libsasl2-modules
 
+postconf -e 'smtpd_sasl_local_domain ='
+postconf -e 'smtpd_sasl_auth_enable = yes'
+postconf -e 'smtpd_sasl_security_options = noanonymous'
+postconf -e 'broken_sasl_auth_clients = yes'
+postconf -e 'smtpd_sasl_authenticated_header = yes'
+postconf -e 'smtpd_recipient_restrictions = permit_sasl_authenticated,permit_mynetworks,reject_unauth_destination'
+postconf -e 'inet_interfaces = all'
+
 touch /etc/postfix/sasl/smtpd.conf
 echo 'pwcheck_method: saslauthd' >> /etc/postfix/sasl/smtpd.conf
 echo 'mech_list: plain login' >> /etc/postfix/sasl/smtpd.conf
+
+# Fiks av rettigheter mellom sasl og postfix
+rm -r /var/run/saslauthd/
+mkdir -p /var/spool/postfix/var/run/saslauthd
+ln -s /var/spool/postfix/var/run/saslauthd /var/run
+chgrp sasl /var/spool/postfix/var/run/saslauthd
+adduser postfix sasl
+
+# Opprette ssl nøkler til postfix
+mkdir /etc/postfix/ssl
+cd /etc/postfix/ssl/
+openssl genrsa -des3 -rand /etc/hosts -out smtpd.key 1024
+chmod 600 smtpd.key
+openssl req -new -key smtpd.key -out smtpd.csr
+openssl x509 -req -days 3650 -in smtpd.csr -signkey smtpd.key -out smtpd.crt
+openssl rsa -in smtpd.key -out smtpd.key.unencrypted
+mv -f smtpd.key.unencrypted smtpd.key
+openssl req -new -x509 -extensions v3_ca -keyout cakey.pem -out cacert.pem -days 3650
+
+postconf -e "myhostname = $HOSTNAVN.$DOMENE"
+postconf -e 'smtpd_tls_auth_only = no'
+postconf -e 'smtp_use_tls = yes'
+postconf -e 'smtpd_use_tls = yes'
+postconf -e 'smtp_tls_note_starttls_offer = yes'
+postconf -e 'smtpd_tls_key_file = /etc/postfix/ssl/smtpd.key'
+postconf -e 'smtpd_tls_cert_file = /etc/postfix/ssl/smtpd.crt'
+postconf -e 'smtpd_tls_CAfile = /etc/postfix/ssl/cacert.pem'
+postconf -e 'smtpd_tls_loglevel = 1'
+postconf -e 'smtpd_tls_received_header = yes'
+postconf -e 'smtpd_tls_session_cache_timeout = 3600s'
+postconf -e 'tls_random_source = dev:/dev/urandom'
+
+mkdir -p /var/spool/postfix/var/run/saslauthd
+sed -i '/START=no/ c\START=yes' /etc/default/saslauthd
+sed -i 's|"-c -m /var/run/saslauthd"|"-c -m /var/spool/postfix/var/run/saslauthd"|g' /etc/default/saslauthd
+
+# Restart sasl og postfix
+/etc/init.d/postfix restart
+/etc/init.d/saslauthd restart
+
+### Legg inn varsling om telnet test og en mulighet til å avbryte om noe skulle være feil
+## tekstsnutt som forklarer telnet bruk og etter test en pause hvor vi git muligheten til å avbryte
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
