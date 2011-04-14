f00aa7d45ea9285de829675bd708bd14b0a3bfb7
Eskil Ugelstad <eskileu@2badr-gr4-m4.(none)>
2011-04-14 08:39:44 +0200

    Små endringer

--- xVariabler.bash
+++ xVariabler.bash
@@ -10,22 +10,23 @@
 # 
 ##
 
-##
+
+# ---------
 # VARIABLER
 # ---------
 # Hardkodingen vil da sørge for at variabler 
 # ikke fylles inn med data fra feks ifconfig
-##
+#
 
 
 # ---WAN--- #
-WAN_IP="" #Om denne hardkodes må også IP1 IP2 IP3 osv hardkodes
+WAN_IP=""   # OBS! Om WAN_IP hardkodes må også W_IP1 W_IP2... hardkodes
 WAN_MASKE=""
 WAN_CIDR=""
 WAN_REV=""
 
 # ---LAN--- #
-LAN_IP=""
+LAN_IP=""   # OBS! Om LAN_IP hardkodes må også L_IP1 L_IP2... hardkodes
 LAN_MASKE=""
 LAN_CIDR=""
 LAN_REV=""
@@ -39,10 +40,14 @@ MASKINNAVN=`hostname`
 # ---TMP---#
 TMPFIL=`date +%N`".tmp"
 ANTBIT=""
-IP1=""
-IP2=""
-IP3=""
-IP4=""
+W_IP1=""
+W_IP2=""
+W_IP3=""
+W_IP4=""
+L_IP1=""
+L_IP2=""
+L_IP3=""
+L_IP4=""
 
 
 echo -n 'Hvilket interface er ditt eksterne| WAN (eth0,eth1,wlan0...): '
@@ -51,12 +56,16 @@ read WAN_IFACE
 echo -n 'Hvilket interface er ditt interne | LAN (eth0,eth1,wlan0...): '
 read LAN_IFACE
 
+
+
+# ---HENT--- #
+# Henter kun om variabler ikke er hardkodet
 if [ -z $WAN_IP ]; then
     WAN_IP=`ifconfig $WAN_IFACE | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-    IP1=`$WAN_IP | cut -d. -f1`
-    IP2=`$WAN_IP | cut -d. -f2`
-    IP3=`$WAN_IP | cut -d. -f3`
-    IP4=`$WAN_IP | cut -d. -f4`
+    W_IP1=`echo $WAN_IP | cut -d. -f1`
+    W_IP2=`echo $WAN_IP | cut -d. -f2`
+    W_IP3=`echo $WAN_IP | cut -d. -f3`
+    W_IP4=`echo $WAN_IP | cut -d. -f4`
 fi
 
 if [ -z $WAN_MASKE ]; then
@@ -64,8 +73,16 @@ if [ -z $WAN_MASKE ]; then
 fi
 
 if [ -z $WAN_CIDR ]; then
-
-    ANTBIT="/"$(mask2cidr $WAN_MASKE)
+    ANTBIT="/"$(mask2cidr $WAN_MASKE) # mas2cidr --> xFunksjoner.bash
+    WAN_CIDR=$W_IP1"."$W_IP2"."$W_IP3".0"$ANTBIT
 fi
+
+if [ -z $WAN_REV ]; then
+    WAN_REV=$W_IP3.$W_IP2.$W_IP1 # Revers for dns
+fi
+
+
+
+
 
 
