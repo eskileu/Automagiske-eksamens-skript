e03b01cde70d2340e2ac70044bf660fed318a710
Eskil Ugelstad <eskileu@2badr-gr4-m4.(none)>
2011-04-14 15:47:14 +0200

    Delt opp filene det er mye lettere og oversiktelig å jobbe på små filer.

--- xVariabler.bash
+++ xVariabler.bash
@@ -1,22 +1,11 @@
-#!/bin/bash 
-. xFunksjoner.bash
-## 
-# Eksperimentel kode som kanskje har en fremtid.
-# En del av experiment branchen til automagiske
-# skript til eksamen. Skilt ut for at de
-# ikke skal kludre til master branch. Merges inn
-# i master når ting er testet godt nok
-# -------------
-# 
+#!/bin/bash
+
 ##
-
-
-# ---------
 # VARIABLER
 # ---------
-# Hardkodingen vil da sørge for at variabler 
+# Hardkodingen vil sørge for at variabler 
 # ikke fylles inn med data fra feks ifconfig
-#
+##
 
 
 # ---WAN--- #
@@ -33,8 +22,8 @@ LAN_REV=""
 
 # ---DIV--- #
 DOMENE=`hostname -d`
-EPOST="ole@sau.no"
-MYSQL_ROOT="l33tw0rd"
+EPOST="ole@sau.no"      # TMP HARDKODET
+MYSQL_ROOT="l33tw0rd"   # TMP HARDKODET
 MASKINNAVN=`hostname`
 
 # ---TMP---#
@@ -48,41 +37,3 @@ L_IP1=""
 L_IP2=""
 L_IP3=""
 L_IP4=""
-
-
-echo -n 'Hvilket interface er ditt eksterne| WAN (eth0,eth1,wlan0...): '
-read WAN_IFACE
-
-echo -n 'Hvilket interface er ditt interne | LAN (eth0,eth1,wlan0...): '
-read LAN_IFACE
-
-
-
-# ---HENT--- #
-# Henter kun om variabler ikke er hardkodet
-if [ -z $WAN_IP ]; then
-    WAN_IP=`ifconfig $WAN_IFACE | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-    W_IP1=`echo $WAN_IP | cut -d. -f1`
-    W_IP2=`echo $WAN_IP | cut -d. -f2`
-    W_IP3=`echo $WAN_IP | cut -d. -f3`
-    W_IP4=`echo $WAN_IP | cut -d. -f4`
-fi
-
-if [ -z $WAN_MASKE ]; then
-    WAN_MASKE=`ifconfig eth0 | grep 'Mask:' | cut -d: -f4 | awk '{ print $1}'`
-fi
-
-if [ -z $WAN_CIDR ]; then
-    ANTBIT="/"$(mask2cidr $WAN_MASKE) # mas2cidr --> xFunksjoner.bash
-    WAN_CIDR=$W_IP1"."$W_IP2"."$W_IP3".0"$ANTBIT
-fi
-
-if [ -z $WAN_REV ]; then
-    WAN_REV=$W_IP3.$W_IP2.$W_IP1 # Revers for dns
-fi
-
-
-
-
-
-
